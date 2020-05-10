4eb8a9726a9dc3cba0d7d9de6c589a221abbc5ee
mujohn26 <mujohn26@gmail.com>
2020-05-10 06:25:44 +0200

    setup folder

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+import logger from './helpers/logger.helper';
+import app from './app';
+
+config(); // This enables dotenv configulations
+
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => logger('info', `Server is running on http://localhost:${port}`));
+export default app;
